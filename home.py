7cbc55a7fc2354188c3c1fa76b0cb2e91cb06e24
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-01 15:46:16 -0300

    primeiro commit

--- home.py
+++ home.py
@@ -1,19 +1,29 @@
 import streamlit as st 
 import pandas as pd
 import query
-
-st.header('Minha dashboard!!')
-
-# sidebar
-
-st.sidebar.image("image/Ferroviario (1).png",use_column_width=True,)
-st.sidebar.header('Home')
-st.sidebar.header('Jogadores')
-st.sidebar.header('Partidas')
+from PIL import Image
 
 result = query.view_all_data()
 df = pd.DataFrame(result,columns=["idpartida","data","competicao","status_jogo","time_adversario","placar","resultado","cartoes_amarelos",
                                   "cartoes_vermelhos","gols_feitos","gols_sofridos","finalizacoes_gol","finalizacoes_totais",
                                   "faltas_cometidas","faltas_recebidas","esquema_tatico","decisao_penalti","finalizacoes_sofridas","esquema_tatico_adv"])
 
-st.dataframe(df)
+df_filtro = df[['data','competicao','time_adversario','resultado']]
+
+st.dataframe(df_filtro)
+
+# SIDEBAR
+
+with st.sidebar:
+    st.header('FERROVIÁRIO')
+    logo = Image.open('image\Ferroviario.png')
+    st.image(logo, use_column_width=True)
+    st.subheader('SELEÇÃO DE FILTROS')
+    fCompeticao = st.selectbox(
+        'Selecione a competição:',
+        options=df['competicao'].unique()
+    )
+    fAdversario = st.selectbox(
+        'Selecione o adversário:',
+        options=df['time_adversario'].unique()
+    )
