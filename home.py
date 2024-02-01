8b366ebbb17d7327a5ab26fae4776bacf640e77e
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-01 17:16:06 -0300

    construindo os filtros no sidebar

--- home.py
+++ home.py
@@ -8,10 +8,10 @@ df = pd.DataFrame(result,columns=["idpartida","data","competicao","status_jogo",
                                   "cartoes_vermelhos","gols_feitos","gols_sofridos","finalizacoes_gol","finalizacoes_totais",
                                   "faltas_cometidas","faltas_recebidas","esquema_tatico","decisao_penalti","finalizacoes_sofridas","esquema_tatico_adv"])
 
-df_filtro = df[['data','competicao','time_adversario','resultado']]
 
-st.dataframe(df_filtro)
 
+colunasUteis = ['data','competicao','time_adversario', 'status_jogo', 'resultado', 'placar']
+df = df[colunasUteis]
 # SIDEBAR
 
 with st.sidebar:
@@ -19,11 +19,32 @@ with st.sidebar:
     logo = Image.open('image\Ferroviario.png')
     st.image(logo, use_column_width=True)
     st.subheader('SELEÇÃO DE FILTROS')
+    default = 'Escolha'
     fCompeticao = st.selectbox(
         'Selecione a competição:',
-        options=df['competicao'].unique()
+        options=df['competicao'].unique(), 
+        index=None,
+        placeholder="Sua escolha...",
+       
     )
     fAdversario = st.selectbox(
         'Selecione o adversário:',
-        options=df['time_adversario'].unique()
+        options=df['time_adversario'].unique(),
+        index=None,
+        placeholder="Sua escolha...",
     )
+    dadosUsuario = None
+    if fCompeticao and fAdversario:
+        dadosUsuario = df.query(
+            'competicao == @fCompeticao & time_adversario == @fAdversario' 
+        )
+    elif fCompeticao and not fAdversario:
+        dadosUsuario = df.query(
+            'competicao == @fCompeticao' 
+        )
+    elif not fCompeticao and fAdversario:
+        dadosUsuario = df.query(
+            'time_adversario == @fAdversario' 
+        )     
+
+st.dataframe(dadosUsuario)
