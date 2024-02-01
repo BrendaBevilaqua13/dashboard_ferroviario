5820446571a3df74515d6883e75473fe732d6549
BrendaBevilaqua13 <brendacarlabevilaqua28@gmail.com>
2024-02-01 20:52:33 -0300

    atualizando o menu

--- home.py
+++ home.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import query
 from PIL import Image
+from streamlit_option_menu import option_menu
+
 
 result = query.view_all_data()
 df = pd.DataFrame(result,columns=["idpartida","data","competicao","status_jogo","time_adversario","placar","resultado","cartoes_amarelos",
@@ -13,13 +15,9 @@ df = pd.DataFrame(result,columns=["idpartida","data","competicao","status_jogo",
 colunasUteis = ['data','competicao','time_adversario', 'status_jogo', 'resultado', 'placar']
 df = df[colunasUteis]
 
-# SIDEBAR
 
-with st.sidebar:
-    st.header('FERROVIÁRIO')
-    logo = Image.open('image\Ferroviario.png')
-    st.image(logo, use_column_width=True)
-    st.subheader('SELEÇÃO DE FILTROS')
+def time():
+    st.title('DESEMPENHO DO TIME')
     default = 'Escolha'
     fCompeticao = st.selectbox(
         'Selecione a competição:',
@@ -50,4 +48,28 @@ with st.sidebar:
     else:
         dadosUsuario = df    
 
-st.dataframe(dadosUsuario)
+    st.header('Resultados')
+    if fCompeticao:
+        st.markdown(f'**Competição selecionada:** {fCompeticao}')
+    if fAdversario:
+        st.markdown(f'**Time Adversário selecionado:** {fAdversario}')
+
+    st.dataframe(dadosUsuario)
+
+
+
+# SIDEBAR
+with st. sidebar:
+    st.header('FERROVIÁRIO')
+    logo = Image.open('image\Ferroviario.png')
+    st.image(logo, use_column_width=True)
+    selected = option_menu(
+        menu_title='Menu',
+        options=['Time','Jogador'],           
+        )
+
+if selected == 'Time':
+    time()
+else:
+    st.title(f'{selected}')
+    
